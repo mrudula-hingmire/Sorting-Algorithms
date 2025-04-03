df36990bbf051d8045bc42324fa2caad7f7b0427
mrudula-hingmire <hingmiremrudula@gmail.com>
2025-04-03 19:58:23 +0530

    Quick_Sort.py

---
+++ Quick_Sort.py
@@ -0,0 +1,12 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+        return quick_sort(left) + middle + quick_sort(right)
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+print("Sorted Array:", quick_sort(arr))
